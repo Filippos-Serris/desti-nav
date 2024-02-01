2439654ddd83ecdb3da6327bbc70264b06fb071a
Filippos-Serris <filipposserris@gmail.com>
2024-02-02 01:08:21 +0200

    detailed object
    set up the detailed object that appear when the user need more info about a place

--- src/components/UI/ObjectModal.jsx
+++ src/components/UI/ObjectModal.jsx
@@ -10,14 +10,26 @@ const Backdrop = (props) => {
 };
 
 const ModalOverlay = (props) => {
-  const {object,closeObject} = props
+  const { object, closeObject } = props;
   const distance = object.distance / 1000;
+
+  const address = `${
+    object.road === undefined ? "Road information not available" : object.road
+  }, ${
+    object.neighbourhood === undefined
+      ? "neighbourhood information not available"
+      : object.neighbourhood
+  }`;
+
+  //object.kinds.replace(/_/g, " ").split(",");
+  const kindsArray = object.kinds.replace(/_/g, " ").split(",");
+
   return (
     <div className="modal">
       <h2 className="name">{object.name}</h2>
 
       <div className="info-container">
-        <p className="object-address">{object.address}</p>
+        <p className="object-address">{address}</p>
         <div className="object-distance">
           <h3>Distance</h3>
           <p>{`${distance.toFixed(1)}km`}</p>
@@ -26,9 +38,13 @@ const ModalOverlay = (props) => {
         <p className="object-rate">{`Rate: ${object.rate}`}</p>
       </div>
 
-      <div className="kinds">
-        <p>{object.kinds}</p>
-      </div>
+      <ul className="kinds-list">
+        {kindsArray.map((data) => (
+          <li key={data} className="kind">
+            {data}
+          </li>
+        ))}
+      </ul>
 
       <a href={object.url} target="_blank">
         Click here fro more info
@@ -39,16 +55,21 @@ const ModalOverlay = (props) => {
 };
 
 const ObjectModal = (props) => {
-  const {object,closeObject} = props
+  const { object, closeObject, objectReady } = props;
   return (
     <Fragment>
       {ReactDOM.createPortal(
         <Backdrop closeObject={closeObject} />,
         document.getElementById("backdrop-root")
       )}
-      {ReactDOM.createPortal(
-        <ModalOverlay object={object} closeObject={closeObject} />,
-        document.getElementById("overlay-root")
+
+      {objectReady ? (
+        ReactDOM.createPortal(
+          <ModalOverlay object={object} closeObject={closeObject} />,
+          document.getElementById("overlay-root")
+        )
+      ) : (
+        <p className="modal">Loading</p>
       )}
     </Fragment>
   );
